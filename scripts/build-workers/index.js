12771b8cceaad6d628db9520770134e9a2342da0
Mannsion <ryan+mannsion@recktek.com>
2025-10-24 21:19:47 -0400

    yml build

--- scripts/build-workers/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Build workers index - exports all build scripts
- */
-
-export { buildDawn } from './buildDawn.js';
-export { buildFFmpeg } from './buildFFmpeg.js';
-export { buildNgixiZigwin32gen } from './buildNgixi_zigwin32gen.js';
-export { buildWasmtime } from './buildWasmtime.js';
